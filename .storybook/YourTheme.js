207710a92e9855125c42a17530c1cb7f74093180
Kiyo <kiyo.nishimura@nano.com.au>
2021-05-06 11:37:39 +1000

    Added themability

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Nano Component Library',
+  brandUrl: 'https://nano.com.au',
+  brandImage: 'https://dev-apply.verteva.com.au/images/Nano_ChatProfile.png',
+  appBg: '#222222',
+  appContentBg: '#333333',
+});
