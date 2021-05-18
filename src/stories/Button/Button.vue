4924ca563b830947db122c86bab4a5421850b63e
Kiyo <kiyo.nishimura@nano.com.au>
2021-05-18 18:13:55 +1000

    - Updates to the button component - Add scss loader to webpack config for SB

--- src/stories/Button/Button.vue
+++ src/stories/Button/Button.vue
@@ -1,13 +1,13 @@
 <template>
-  <button
-    type="button" 
-    class="m-4"
-    :class="classes"
-    @click="onClick"
-    :style="style"
-  >
-    {{ label }}
-  </button>
+    <button
+      type="button" 
+      class="m-4 photon-btn"
+      :class="classes"
+      @click="onClick"
+      :style="style"
+    >
+     {{ label }}
+    </button>
 </template>
 
 <script lang="ts">
@@ -21,7 +21,7 @@ interface Props {
 }
 
 export default defineComponent({
-  name: 'button',
+  name: 'PButton',
 
   props: {
     label: {
@@ -38,9 +38,6 @@ export default defineComponent({
         return ['small', 'medium', 'large'].indexOf(value) !== -1;
       },
     },
-    backgroundColor: {
-      type: String,
-    },
   },
 
   emits: ['click'],
@@ -48,17 +45,17 @@ export default defineComponent({
   setup(_: Props, { emit }: {
     emit: (a: string) => void;
   }): () => {
-    classes: unknown;
-    style: unknown;
-    onClick: unknown;
+    classes: any;
+    style: any;
+    onClick: any;
   } {
     const props = reactive(_);
     return {
       classes: computed(() => ({
-        'storybook-button': true,
-        'storybook-button--primary': props.primary,
-        'storybook-button--secondary': !props.primary,
-        [`storybook-button--${props.size || 'medium'}`]: true,
+        'photon-btn': true,
+        'photon-btn--primary': props.primary,
+        'photon-btn--secondary': !props.primary,
+        [`photon-btn--${props.size || 'medium'}`]: true,
       })),
       style: computed(() => ({
         backgroundColor: props.backgroundColor,
@@ -70,3 +67,52 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss">
+  .photon-btn {
+    background: none;
+    border: 0;
+    border-radius: 2rem;
+    color: white;
+    cursor: pointer;
+    transition: .3s;
+
+    &--primary {
+      background: #333333;
+      
+       &:hover {
+        &:not(:disabled) {
+          background: lighten(#333333, 20%);
+        }
+       }
+    }
+    &--secondary {
+      background: #999999;
+      
+      &:hover {
+        &:not(:disabled) {
+          background: lighten(#999999, 20%);
+        }
+      }
+    }
+
+    &--small {
+      font-size: 12px;
+      padding: 0.5rem 1rem;
+    }
+    &--medium {
+      font-size: 16px;
+      padding: 0.75rem 1.5rem;
+    }
+    &--large {
+      font-size: 20px;
+      padding: 1rem 2rem;
+    }
+
+    &:disabled {
+      cursor: not-allowed;
+      opacity: 0.2;
+    }
+  }
+
+</style>
