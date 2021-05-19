1a2a5e7f5dea77a5345d2189168884efd2a9a838
Chris <chris.rodriguez@nano.com.au>
2021-05-19 14:35:24 +1000

    add lib build scripts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import './assets/tailwind.css';
-import vSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css';
-import { createApp } from 'vue';
-import App from './App.vue';
+import Button from './stories/Button';
 
-const app = createApp(App);
-
-app.component('v-select', vSelect);
-app.mount('#app');
+export default Button;
