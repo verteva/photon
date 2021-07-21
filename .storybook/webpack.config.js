56eb2d90475ac01c0ecab0e9068fb11a753082d9
Kiyo <kiyo.nishimura@nano.com.au>
2021-07-21 16:59:43 +1000

    Cleanup configs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,25 +10,7 @@ module.exports = ({ config }) => {
         require.resolve('sass-loader'),
       ],
       include: path.resolve(__dirname, '../'),
-    })
-    
-    // let rule = config.module.rules.find(r =>
-    //     // it can be another rule with file loader 
-    //     // we should get only svg related
-    //     r.test && r.test.toString().includes('svg') &&
-    //     // file-loader might be resolved to js file path so "endsWith" is not reliable enough
-    //     r.loader && r.loader.includes('file-loader')
-    // );
-    // rule.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
-
-    // config.module.rules.push(
-    //     {
-    //         test: /\.svg$/,
-    //         use: ['vue-svg-loader']
-    //     }
-    // )
-
-  
+    });  
 
   return config;
 };
