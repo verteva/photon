f14bc5c5691d05da71e1eeb3e926e0d7dd8c8c7b
Kiyo <kiyo.nishimura@nano.com.au>
2021-07-26 14:50:44 +1000

    Add additional components for headings

--- src/stories/Typography/Typography.stories.js
+++ src/stories/Typography/Typography.stories.js
@@ -1,4 +1,7 @@
 import PText from './Text.vue';
+import PH1 from './H1.vue';
+import PH2 from './H2.vue';
+import PH3 from './H3.vue';
 
 import {
   TextElementDiv,
@@ -34,15 +37,43 @@ Text.args = {
   xs: false,
 };
 
+const H1Template = (args, { argTypes }) => ({
+  components: { PH1 },
+  props: Object.keys(argTypes),
+  template: `<p-h1>${args.copy}</p-h1>`,
+});
+export const H1 = H1Template.bind({});
+H1.args = {
+  copy: 'Heading 1',
+};
+
+const H2Template = (args, { argTypes }) => ({
+  components: { PH2 },
+  props: Object.keys(argTypes),
+  template: `<p-h2>${args.copy}</p-h2>`,
+});
+export const H2 = H2Template.bind({});
+H2.args = {
+  copy: 'Heading 2',
+};
+
+const H3Template = (args, { argTypes }) => ({
+  components: { PH3 },
+  props: Object.keys(argTypes),
+  template: `<p-h3>${args.copy}</p-h3>`,
+});
+export const H3 = H3Template.bind({});
+H3.args = {
+  copy: 'Heading 3',
+};
+
 const AllTemplate = () => ({
-  components: { PText },
+  components: { PText, PH1, PH2, PH3 },
   template: `
     <div>
-      (font-size  / line height / letter spacing)
-      <hr class="ph-my-6" />
-      <h1 class="ph-my-10">This an H1 heading<br /> with a second line</h1>
-      <h2 class="ph-my-10">This an H2 heading<br /> with a second line</h2>
-      <h3 class="ph-my-10">This an H3 heading<br /> with a second line</h3>
+      <p-h1 class="ph-my-10">This an H1 heading<br /> with a second line</p-h1>
+      <p-h2 class="ph-my-10">This an H2 heading<br /> with a second line</p-h2>
+      <p-h3 class="ph-my-10">This an H3 heading<br /> with a second line</p-h3>
       <p-text xl class="ph-my-10">This is Body XLarge<br />and another line</p-text>
       <p-text lg class="ph-my-10">This is Body Large<br />and another line</p-text>
       <p-text class="ph-my-10">This is Body <br />and another line</p-text>
--- src/stories/Typography/index.ts
+++ src/stories/Typography/index.ts
@@ -1,4 +1,12 @@
 import PText from './Text.vue';
+import PH1 from './H1.vue';
+import PH2 from './H2.vue';
+import PH3 from './H3.vue';
 
-export default PText;
+export default {
+  PText,
+  PH1,
+  PH2,
+  PH3,
+};
 
