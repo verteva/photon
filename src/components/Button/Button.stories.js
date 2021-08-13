06bc58d3ca4afaa2a37a45e9742057082dce78dc
Kiyo <kiyo.nishimura@nano.com.au>
2021-08-13 14:36:08 +1000

    Button loader fix for alignment

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,8 +7,8 @@ export default {
   argTypes: {
     disabled: { options: [true, false] },
     submitting: { options: [true, false] },
-    size: { type: 'select', options: ['xs', 'small', 'medium', 'large'] },
-    buttonStyle: { type: 'select', options: ['primary', 'secondary', 'plain'] },
+    // size: { type: 'select', options: ['xs', 'small', 'medium', 'large'] },
+    // buttonStyle: { type: 'select', options: ['primary', 'secondary', 'plain'] },
     type: { type: 'select', options: ['submit', 'button'] },
     onClick: {},
   },
@@ -19,31 +19,47 @@ export default {
   },
 };
 
-const Template = (args, { argTypes }) => ({
+const TypesTemplate = (args, { argTypes }) => ({
   components: { PButton },
   props: Object.keys(argTypes),
-  template: '<PButton v-bind="$props" />',
+  template: `
+    <div class="ph-flex">
+      <div class="ph-flex ph-flex-col ph-mr-10">
+        <PButton v-bind="$props" buttonStyle="primary" size="medium"/>
+        <br />
+        <PButton v-bind="$props" buttonStyle="primary" size="small"/>
+        <br />
+        <PButton v-bind="$props" buttonStyle="primary" size="xs" />
+        </div>
+      <div class="ph-flex ph-flex-col">
+        <PButton v-bind="$props" buttonStyle="secondary" size="medium"/>
+        <br />
+        <PButton v-bind="$props" buttonStyle="secondary" size="small"/>
+        <br />
+        <PButton v-bind="$props" buttonStyle="secondary" size="xs" />
+      </div>
+    </div>
+  `,
 });
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Types = TypesTemplate.bind({});
+Types.args = {
   disabled: false,
   submitting: false,
-  block: false,
   buttonStyle: 'primary',
   size: 'small',
   type: 'submit'
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  disabled: false,
-  label: 'Button',
-  buttonStyle: 'secondary',
-  size: 'small',
-};
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   disabled: false,
+//   label: 'Button',
+//   buttonStyle: 'secondary',
+//   size: 'small',
+// };
 
-export const Plain = Template.bind({});
-Plain.args = {
-  buttonStyle: 'plain',
-};
+// export const Plain = Template.bind({});
+// Plain.args = {
+//   buttonStyle: 'plain',
+// };
