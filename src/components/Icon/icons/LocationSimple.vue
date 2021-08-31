f0b14a702d946410d423308fa40eda9a084ca9cc
Kiyo <kiyo.nishimura@nano.com.au>
2021-08-31 17:02:39 +1000

    Updated to AutoComplete

--- src/components/Icon/icons/LocationSimple.vue
+++ src/components/Icon/icons/LocationSimple.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg
+<svg
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     aria-hidden="true"
@@ -10,6 +10,7 @@
     viewBox="0 0 24 24"
   ><path
     d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7z"
-    fill="currentColor"
+    stroke="currentColor"
+    fill="none"
   ></path></svg>
 </template>
