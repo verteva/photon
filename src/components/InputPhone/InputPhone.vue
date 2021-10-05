83220c2d2d40e7d273c1f45f7be05c59ee6a00dd
rmnano <ryan.meng@nano.com.au>
2021-10-05 14:50:54 +1100

    add border brand2 when focus
    add border brand2 when focus

--- src/components/InputPhone/InputPhone.vue
+++ src/components/InputPhone/InputPhone.vue
@@ -182,9 +182,12 @@ input::-webkit-inner-spin-button {
 }
 .vue-phone-number-input{
   border-radius: var(--borderRadius, 12px);
+  @apply ph-border;
+  @apply ph-border-transparent;
 }
 .vue-phone-number-input:focus-within{
   box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%), 0 0 8px rgb(102 175 233 / 60%);
+  @apply ph-border-brand2;
 }
 .country-selector__input, .input-tel__input{
   box-shadow: none!important;
