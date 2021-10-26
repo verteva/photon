294d302b6987b433ef4168251cb107b2dd7817fc
rmnano <ryan.meng@nano.com.au>
2021-10-26 11:36:01 +1100

    badge and unit:test
    badge and unit:test

--- src/components/Badge/Badge.stories.js
+++ src/components/Badge/Badge.stories.js
@@ -1,4 +1,6 @@
 import PBadge from './Badge.vue';
+import PIcon from '../Icon';
+import PButton from '../Button';
 
 export default {
   title: 'Components/Badge',
@@ -8,15 +10,49 @@ export default {
 };
 
 const Template = (args, { argTypes }) => ({
-  components: {PBadge},
+  components: {PBadge, PIcon, PButton},
   props: Object.keys(argTypes),
+  data() {
+    return {
+      value: 0
+    }
+  },
   template: `
-    <div>
-      <PBadge v-bind='$props'>
-        Test
-      </PBadge
+    <div class="ph-mt-4">
+      <div>
+        <p-badge class="ph-text-brand2" :show="showBadge" v-bind='$props'>
+          <p-icon
+            name="House"
+            type="lg">
+          </p-icon>
+          <template #badge>
+          {{ value }}
+          </template>
+        </p-badge>
+      </div>
+      <div class="ph-mt-4">
+        <p-button @click="sendNotification">
+          Send Notification
+        </p-button>
+        <p-button type="plain" @click="clearNotification">
+          Clear Notification
+        </p-button>
+      </div>
     </div>
   `,
+  computed:{
+    showBadge() {
+      return this.value !== 0;
+    }
+  },
+  methods: {
+    sendNotification() {
+      this.value++;
+    },
+    clearNotification() {
+      this.value = 0;
+    }
+  }
 });
 
 export const Badge = Template.bind({});
--- src/components/Badge/Badge.vue
+++ src/components/Badge/Badge.vue
@@ -1,20 +1,33 @@
 <template>
-  <div>
-    <slot>
-    </slot>
-    <span class="photon-badge__wrapper">
+  <span
+    class="photon-badge"
+    :class="classList"
+  >
+    <span>
+      <slot>
+      </slot>
+    </span>
+    <span
+      ref="badgeWrapper"
+      class="photon-badge__wrapper"
+      :style="{
+        '--position': inline ? 'relative':'absolute',
+        '--width': width,
+        '--height': height,
+        '--radius': tile? '0px': borderRadius,
+        '--inset': inset,
+      }"
+    >
       <span
-        class="photon-badge"
-        :class="baseClassList"
-        :style="{
-          '--width': width,
-          '--height': height,
-          '--radius': tile? '0px': borderRadius,
-        }"
+        ref="badge"
+        class="photon-badge__badge"
+        :class="badgeClassList"
       >
+        <slot name="badge">
+        </slot>
       </span>
     </span>
-  </div>
+  </span>
 </template>
 
 <script lang="ts">
@@ -55,25 +68,99 @@ export default Vue.extend({
       type: String as PropType<string>,
       default: '100%',
     },
+    bgColorClass: {
+      type: String as PropType<string>,
+      default: 'ph-bg-brand2',
+    },
+    badgeTextSize: {
+      type: String as PropType<string>,
+      default: 'xs',
+    },
+    show: {
+      type: Boolean as PropType<boolean>,
+      default: false,
+    }
   },
 
-
-  data(): any {    
-    return {
-      baseClassList: [
-        'ph-absolute',
+  computed: {
+    badgeClassList(): string[] {
+      return [
+        'ph-items-center',
+        'ph-inline-block',
+        'ph-bg-current',
+        'ph-text-white',
+        'ph-items-center',
+        'ph-justify-center',
         'ph-text-center',
-        'ph-bg-brand2',
-        'ph-whitespace-nowrap',
-      ],
-    };
-  },
+        'ph-p-1',
+        'ph-origin-center',
+        this.bgColorClass as string,
+        'ph-text-'+this.badgeTextSize,
+        'ph-leading-none',
+        'ph-transform',
+        this.show ? 'photon-badge--show':'photon-badge--hide',
+      ];
+    },
+    classList(): string[] {
+      const a: string[] = [
+        ...this.baseClassList as string,
+        this.inline && "photon-badge--inline",
+      ];
+      return a;
+    },
+    baseClassList(): string[] {
+      return [
+        'ph-items-center',
+      ];
+    },
+    inset() { 
+      const calc = this.overlap? ' calc(100% - 12px)' : ' calc(100% - 4px)';
+      return !this.inline && (this.bottom ? calc : ' auto') + (this.left ? calc : ' auto') + (this.bottom ? ' auto' : calc) + (this.left ? ' auto' : calc);
+    }
+  }
 });
 </script>
 <style lang="postcss" scoped>
-.photon-badge{
+.photon-badge {
+  @apply ph-inline-block;
+  @apply ph-relative;
+}
+.photon-badge__wrapper {
+  @apply ph-w-full;
+  @apply ph-h-full;
+  @apply ph-top-0;
+  @apply ph-left-0;
+  @apply ph-pointer-events-none;
+  @apply ph-flex-1;
+  @apply ph-flex-grow-0;
+  position: var(--position, absolute);
+}
+.photon-badge__badge {  
+  position: var(--position, absolute);
+  min-width: 20px;
   width: var(--width, 20px);
   height: var(--height, 20px);
   border-radius: var(--radius, 100%);
+  inset: var(--inset, 'auto auto calc(100% - 4px) calc(100% - 4px)');
+  transition: .3s cubic-bezier(.25,.8,.5,1);
+    transition-property: all;
+    transition-duration: 0.3s;
+    transition-timing-function: cubic-bezier(0.25, 0.8, 0.5, 1);
+    transition-delay: 0s;
+}
+.photon-badge--inline {
+  @apply ph-inline-flex;
+  @apply ph-items-center;
+  @apply ph-justify-center; 
+}
+.photon-badge--hide {
+  @apply ph-opacity-0;
+  --tw-scale-x: 0!important;
+  --tw-scale-y: 0!important;
+}
+.photon-badge--show {
+  @apply ph-opacity-100;
+  --tw-scale-x: 1!important;
+  --tw-scale-y: 1!important;
 }
 </style>
