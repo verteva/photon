95f430a3c3824d757bf70a4034dfb13d70a23720
rmnano <ryan.meng@nano.com.au>
2021-10-29 16:09:34 +1100

    new icons for nav
    new icons for nav

---
+++ src/components/Icon/icons/Ecllipse.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+  >
+    <circle cx="16.3086" cy="15" r="11" stroke="currentColor" />
+  </svg>
+</template>
