7e808158b731150ea915b0b722aac63a206b1a84
Todd-j-sutherland <89565426+Todd-j-sutherland@users.noreply.github.com>
2021-11-26 10:46:32 +1100

    checkbox unit tests

---
+++ src/components/Checkbox/checkbox.spec.js
@@ -0,0 +1,42 @@
+import { createWrapper } from '@/utils/unitTest.ts';
+import Checkbox from '@/components/Checkbox/Checkbox.vue';
+
+describe('ContentCard.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = createWrapper(Checkbox);
+  });
+
+  it('check slot: label slot is setup', async () => {
+    const checkboxLabel = wrapper.html();
+    expect(checkboxLabel).toContain('Slot Content');
+  });
+
+  it('check props: label is setup', async () => {
+    const checkboxLabel = 'Hello world';
+    await wrapper.setProps({ checkboxLabel });
+    const checkboxHTML = wrapper.html();
+    expect(checkboxLabel).toEqual(checkboxLabel);
+    expect(checkboxHTML).toContain(checkboxLabel);
+  });
+
+  it('check props: DarkBoarder is setup', async () => {
+    const darkBorder = true;
+    await wrapper.setProps({ darkBorder });
+    const inputField = wrapper.find('.ph-checkbox-toggle');
+    expect(inputField.classes()).toContain('ph-border-grey4');
+  });
+
+  it('check prop: input field checkbox is checked', async () => {
+    const checkbox = wrapper.find('input[type="checkbox"]');
+    await checkbox.setChecked();
+    expect(checkbox.element.checked).toBeTruthy();
+  });
+
+  it('check props: value change the HTML', async () => {
+    const value = true;
+    await wrapper.setProps({ value });
+    const checkboxTick = wrapper.get('.ph-checkbox-toggle > div');
+    expect(checkboxTick.classes()).toContain('ph-opacity-1');
+  });
+});
