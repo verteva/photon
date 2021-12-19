ae018aeb571241174601221e807977b27101fd09
Todd-j-sutherland <89565426+Todd-j-sutherland@users.noreply.github.com>
2021-12-20 10:53:16 +1100

    Cleaning test file code

--- src/components/Message/Message.spec.js
+++ src/components/Message/Message.spec.js
@@ -1,36 +1,37 @@
 import Message from '@/components/Message/Message.vue';
-// import { createWrapper } from '@/utils/unitTest';
-import { mount, createLocalVue } from '@vue/test-utils';
-
-const localVue = createLocalVue();
-const onClick = jest.fn();
+import { createWrapper } from '@/utils/unitTest';
 
 describe('Message.vue', () => {
   let wrapper;
+  let button;
+  let iconWrapper;
   beforeEach(() => {
-    wrapper = mount(Message, {
-      listeners: {
-        click: onClick,
-      },
-      localVue,
-    });
+    wrapper = createWrapper(Message);
+    button = wrapper.getComponent({ ref: 'messageButton' });
+    iconWrapper = wrapper.getComponent({ ref: 'ph-message' });
   });
 
   it('check prop: callToAction', async () => {
     const callToAction = 'click me!';
+
     await wrapper.setProps({ callToAction });
+
     expect(wrapper.props().callToAction).toEqual(callToAction);
   });
 
   it('check prop: title', async () => {
     const title = 'this is a title';
+
     await wrapper.setProps({ title });
+
     expect(wrapper.props().title).toEqual(title);
   });
 
   it('check prop: description', async () => {
     const description = 'this is a description';
+
     await wrapper.setProps({ description });
+
     expect(wrapper.props().description).toEqual(description);
   });
 
@@ -40,12 +41,10 @@ describe('Message.vue', () => {
     const type = 'warning';
     await wrapper.setProps({ type });
     expect(wrapper.props().type).toEqual(type);
-    const computedIcon = wrapper.vm.typeStyle;
 
+    const computedIcon = wrapper.vm.typeStyle;
     const iconClasses = computedIcon.classes;
     const icontype = computedIcon.icon;
-
-    const iconWrapper = wrapper.getComponent({ ref: 'ph-message' });
 
     expect(iconWrapper.classes()).toContain(iconClasses[0]);
     expect(iconWrapper.classes()).toContain(iconClasses[1]);
@@ -54,10 +53,12 @@ describe('Message.vue', () => {
 
   it('emit event: button click, show button when description', async () => {
     expect(wrapper.emitted('click')).toBeFalsy();
+
     const description = 'show button';
+
     await wrapper.setProps({ description });
-    const button = wrapper.getComponent({ ref: 'messageButton' });
     await button.trigger('click');
+
     expect(wrapper.emitted('click')).toBeTruthy();
   });
 });
