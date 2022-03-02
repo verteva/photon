f184059b1f0a4a2ac56b673008c66fa2feee5f42
Zoe Chai <zoechai@Ozeits-MacBook-Pro-4.local>
2022-03-02 15:18:47 +1100

    loanfigures component working progress unit test

---
+++ src/components/LoanFigures/LoanFigures.stories.js
@@ -0,0 +1,25 @@
+import PLoanFigures from './LoanFigures.vue';
+
+export default {
+  title: 'Components/LoanFigures',
+  component: PLoanFigures,
+  argTypes: {
+    // Configurable component options in SB UI...
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { PLoanFigures },
+  props: Object.keys(argTypes),
+  template: "<PLoanFigures v-bind='$props' />",
+});
+
+export const CurrentLoan = Template.bind({});
+CurrentLoan.args = {
+  background: 'ph-bg-brand1',
+  title: 'Current Loan',
+  loading: false,
+  darkMode: false,
+  name: 'Rent',
+  value: 200000,
+};
---
+++ src/components/LoanFigures/types.ts
@@ -0,0 +1,6 @@
+export interface LoanFiguresData {
+  calculation: number;
+  calcInterval: number;
+  min: number;
+  max: number;
+}
