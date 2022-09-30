e45af431979f5c5d16c4ffc1c2dac0018cd26a1a
Dan Drory <dan.drory@nano.com.au>
2022-09-30 14:38:22 +1000

    fix: added additional parser types and fixed shadow parser

--- src/utils/figmaTheme/templateResolvers/parsers/boxShadowParser.js
+++ src/utils/figmaTheme/templateResolvers/parsers/boxShadowParser.js
@@ -4,15 +4,19 @@ export const boxShadowParser = (value, root) => {
   if (typeof value.value === 'string') {
     return replaceTemplateValue(value.value, root);
   }
-  const mappedStrings = value.value.map(
-    ({ blur, color, spread, type, x, y }) =>
-      `${replaceTemplateValue(x, root)}px ${replaceTemplateValue(
-        y,
-        root
-      )}px ${replaceTemplateValue(blur, root)}px ${replaceTemplateValue(
-        spread,
-        root
-      )}px ${replaceTemplateValue(color, root)}`
-  );
-  return `${mappedStrings.join(', ')}`;
+
+  const buildShadow = ({ blur, color, spread, type, x, y }) =>
+    `${replaceTemplateValue(x, root)}px ${replaceTemplateValue(
+      y,
+      root
+    )}px ${replaceTemplateValue(blur, root)}px ${replaceTemplateValue(
+      spread,
+      root
+    )}px ${replaceTemplateValue(color, root)}`;
+
+  if (Array.isArray(value.value)) {
+    const mappedStrings = value.value.map(buildShadow);
+    return `${mappedStrings.join(', ')}`;
+  }
+  return buildShadow(value.value);
 };
