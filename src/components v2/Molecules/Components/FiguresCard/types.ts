6ba1ed25f1d29663405cca9903afe6190a5d7ec3
rmnano <ryan.meng@nano.com.au>
2022-12-12 16:08:36 +1100

    fix: update unused FiguresData

--- src/components v2/Molecules/Components/FiguresCard/types.ts
+++ src/components v2/Molecules/Components/FiguresCard/types.ts
@@ -1,10 +1,3 @@
-export interface FiguresData {
-  calculation: number;
-  calcInterval: number;
-  min: number;
-  max: number;
-}
-
 export const CardStyles = {
   PRIMARY: 'primary',
   SECONDARY: 'secondary',
