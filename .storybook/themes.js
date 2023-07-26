9f2f93d6703916f8cf957c9e49e6afef1600456b
Simon Foxe <simon.foxe@nano.com.au>
2023-07-27 09:31:08 +1000

    chore: update storybook brand settings

--- .storybook/themes.js
+++ .storybook/themes.js
@@ -3,8 +3,8 @@ import { create } from '@storybook/theming';
 export const dark = create({
   base: 'dark',
   brandTitle: 'Nano Component Library',
-  brandUrl: 'https://nano.com.au',
-  brandImage: 'https://dev-apply.verteva.com.au/images/Nano_ChatProfile.png',
+  brandUrl: 'https://nanolendingsolutions.com',
+  brandImage: '',
   appBg: '#222222',
   appContentBg: '#333333',
 });
@@ -12,8 +12,8 @@ export const dark = create({
 export const light = create({
   base: 'light',
   brandTitle: 'Nano Component Library',
-  brandUrl: 'https://nano.com.au',
-  brandImage: 'https://dev-apply.verteva.com.au/images/Nano_ChatProfile.png',
+  brandUrl: 'https://nanolendingsolutions.com',
+  brandImage: '',
   // UI
   appBg: '#ffffff',
   appContentBg: '#efefef',
