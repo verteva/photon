6795cbbad1ec9a01c45339df78f918fb7fd94409
Simon Foxe <simon.foxe@nano.com.au>
2023-07-26 14:09:07 +1000

    chore: disable all old theme methods and use new chameleon sd vars

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,78 +4,29 @@ import '../src/assets/css/main.css';
 import Vuex from 'vuex';
 import Vue from 'vue';
 import store from '@/components/store';
-import externalStore from '@/components v2/store';
-import {
-  flattenObjectToCssVars,
-  parseBrandingJson,
-} from '@/utils/parseBrandingJson';
-import { injectThemeCssVariables } from '@/utils/injectThemeCssVariables';
-import { getTheme } from './utils/getTheme';
-import { getThemeNames } from './utils/getThemeNames';
+//import externalStore from '@/components v2/store';
+import { injectThemeCSSVariablesFromFile } from '@/utils/injectThemeCssVariables';
 import { ThemeBar } from './themeBar';
 import '@/utils/fontAwesome/setup';
 import './theme-bar.scss';
 
-const isExternal = process.env.STORYBOOK_THEME_LOCATION === 'external';
-const isFigma = process.env.STORYBOOK_THEME_LOCATION === 'figma';
-
-const getThemeFiles = () => {
-  const localFiles = require.context('../src/theme/', true, /theme\.json$/i);
-  if (!isExternal) {
-    return localFiles;
-  }
-  try {
-    const { getFiles } = require('@/utils/themeFileLocation');
-    return getFiles();
-  } catch (e) {
-    alert(
-      "Could not load themeFileLocation.js. Make sure you've copied the file. Check the readme.md for more information. Using default theme instead."
-    );
-    console.error(
-      "Could not load themeFileLocation.js. Make sure you've copied the file. Check the readme.md for more information. Using default theme instead."
-    );
-    return localFiles;
-  }
-};
-
 Vue.use(Vuex);
-Vue.prototype.$store = isExternal ? externalStore : store;
+Vue.prototype.$store = store;
 
 export const withTheme = (story) => {
   return {
     components: { story, ThemeBar },
     template:
-      isExternal || isFigma
-        ? '<ThemeBar :theme="theme" :theme-names="themeNames" @input="setThemeName" :color="color"><story /></ThemeBar>'
-        : '<story />',
+      '<ThemeBar :theme="theme" :theme-names="themeNames" @input="setThemeName" :color="color"><story /></ThemeBar>',
     data() {
       return {
         color: 'black',
         themeObject: {},
-        themeNames: ['Nano'],
-        theme: 'Nano',
+        themeNames: ['amp', 'demo'],
+        theme: 'amp',
       };
     },
-    async created() {
-      if (isFigma) {
-        try {
-          const res = await fetch(
-            `${process.env.STORYBOOK_THEME_URL}/themes.json`
-          );
-          const themesJson = await res.json();
-          this.themeNames = themesJson.themes;
-        } catch (e) {
-          console.error('cant find theme names', e);
-        }
-      }
-      if (isExternal) {
-        const files = getThemeFiles();
-        this.themeNames = getThemeNames(files);
-        if (store._modules.root._children.theme.state.themeName) {
-          this.theme = store._modules.root._children.theme.state.themeName;
-        }
-      }
-    },
+    async created() {},
     methods: {
       setThemeName(themeName) {
         this.theme = themeName.target.value;
@@ -85,30 +36,10 @@ export const withTheme = (story) => {
     watch: {
       theme: {
         async handler(val) {
-          if (isFigma) {
-            const loadFigmaTheme = async () => {
-              const res = await fetch(
-                `${process.env.STORYBOOK_THEME_URL}/${val}.json`
-              );
-              const parsedTheme = await res.json();
-              this.color = parsedTheme?.theme?.primary?.default;
-              injectThemeCssVariables(flattenObjectToCssVars(parsedTheme));
-              store.dispatch('theme/setTheme', parsedTheme);
-            };
-
-            loadFigmaTheme();
-            return;
-          }
-          try {
-            const files = getThemeFiles();
-            const json = await getTheme(files, isExternal ? val : '');
-            const loadedTheme = parseBrandingJson(json);
-            this.color = loadedTheme?.theme?.primary?.default;
-            injectThemeCssVariables(flattenObjectToCssVars(loadedTheme));
-            store.dispatch('theme/setTheme', loadedTheme);
-          } catch (e) {
-            console.error('Error: Unable to load and inject theme, errors:', e);
-          }
+          // Load theme CSS variables
+          injectThemeCSSVariablesFromFile(
+            `/branding/${val}/tokens/css/variables-refs.css`
+          );
         },
         immediate: true,
       },
@@ -117,7 +48,7 @@ export const withTheme = (story) => {
 };
 
 export const parameters = {
-  layout: isFigma || isExternal ? 'fullscreen' : 'padded',
+  layout: 'fullscreen',
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
