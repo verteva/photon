52b2cdf864fd78dc5713d84ed85751d4c4309fae
Simon Foxe <simon.foxe@nano.com.au>
2023-07-29 15:26:32 +1000

    feat: input date uplift and remap, auto sibling focus, date format support

--- src/components v2/Atoms/Forms/InputDate/InputDate.vue
+++ src/components v2/Atoms/Forms/InputDate/InputDate.vue
@@ -5,39 +5,40 @@
       name="day"
       placeholder="DD"
       centered
+      class="ph-input-date-field ph-input-date-field-day"
       :maxlength="2"
       :value="value.day"
       :disabled="disabled"
       is-number
       @input="$emit('dateDay', $event)"
+      @keyup="onKeyup"
       @blur="$emit('dateDayBlur', $event)"
     />
-    <div class="ph-date-slash">/</div>
-
     <p2-input-text
       type="text"
       name="month"
       placeholder="MM"
       centered
+      class="ph-input-date-field ph-input-date-field-month"
       :maxlength="2"
       :value="value.month"
       :disabled="disabled"
       is-number
       @input="$emit('dateMonth', $event)"
+      @keyup="onKeyup"
       @blur="$emit('dateMonthBlur', $event)"
     />
-
-    <div class="ph-date-slash">/</div>
-
     <p2-input-text
       name="year"
       placeholder="YYYY"
       centered
+      class="ph-input-date-field ph-input-date-field-year"
       :maxlength="4"
       :value="value.year"
       :disabled="disabled"
       is-number
       @input="$emit('dateYear', $event)"
+      @keyup="onKeyup"
       @blur="$emit('dateYearBlur', $event)"
     />
   </div>
@@ -65,6 +66,27 @@ export default Vue.extend({
   },
 
   props,
+
+  methods: {
+    onKeyup: function (e: InputEvent) {
+      // If key entered is a number
+      const isNumber = isFinite(event.key);
+      if (isNumber) {
+        // Check if total length in field equals maxlength
+        if (e.target.value.length >= e.target.maxLength) {
+          // If field has a next sibling (ie: it's not last field)
+          if (e.target.parentElement.nextElementSibling) {
+            // Give focus to the next sibling's input field and select all text
+            let sibling = e.target.parentElement.nextElementSibling;
+            if (sibling.children[0]) {
+              sibling.children[0].focus();
+              sibling.children[0].select();
+            }
+          }
+        }
+      }
+    },
+  },
 });
 </script>
 
@@ -72,10 +94,42 @@ export default Vue.extend({
 .ph-input-date-wrapper {
   display: flex;
   align-items: center;
-  width: 320px;
+  --input-width: 56px;
+  --input-slash-padding: 1.25em;
+
+  .ph-input-text {
+    position: relative;
+    padding-right: var(--input-slash-padding);
+
+    &:after {
+      content: '/';
+      font-size: 16px;
+      color: var(--sd-theme-fg-default);
+      position: absolute;
+      right: 0px;
+      top: 0;
+      bottom: 0;
+      margin: auto;
+      width: 12px;
+      height: 1em;
+      line-height: 1;
+    }
+
+    &:last-child {
+      --input-slash-padding: 0px;
+      &:after {
+        content: none;
+      }
+    }
+  }
 }
-.ph-date-slash {
-  margin: var(--input-date-divider-margin);
-  color: var(--input-date-divider-color);
+
+.ph-input-date-field {
+  width: calc(var(--input-width) + var(--input-slash-padding));
+
+  &.ph-input-date-field-year {
+    --input-width: 64px;
+    //width: 64px;
+  }
 }
 </style>
