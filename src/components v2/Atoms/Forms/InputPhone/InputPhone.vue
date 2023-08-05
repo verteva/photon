e56804f8018a9ad7af88fa2130ff10b54ae644f5
Simon Foxe <simon.foxe@nano.com.au>
2023-08-06 00:13:42 +1000

    feat: input phone uplift and remap

--- src/components v2/Atoms/Forms/InputPhone/InputPhone.vue
+++ src/components v2/Atoms/Forms/InputPhone/InputPhone.vue
@@ -19,7 +19,11 @@
       @input="$emit('input', $event)"
       @phone-number-focused="onFocus"
       @phone-number-blur="onBlur"
-    />
+    >
+      <template #arrow>
+        <font-awesome-icon :icon="['fal', 'chevron-down']" class="fa-xs" />
+      </template>
+    </VuePhoneNumberInput>
   </div>
 </template>
 
@@ -87,111 +91,86 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-@import '~vue-phone-number-input/dist/vue-phone-number-input.css';
+.vue-phone-number-input {
+  --borderRadius: var(--sd-input-md-border-radius);
 
-.ph-phone-input-component {
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-  .country-selector__country-flag {
-    top: 16px !important;
-  }
-  .country-selector__input {
-    padding-top: 0 !important;
-  }
-  .input-tel__input {
-    border-left-width: 0px !important;
-  }
-  .select-country-container {
-    flex: 0 0 60px !important;
-    width: 60px !important;
-    min-width: auto !important;
-  }
-  .country-selector__input {
-    width: 0px !important;
-  }
-  .country-selector__input {
-    border-right-width: 0px !important;
+  position: relative;
+  transition-property: background-color, border-color, color, fill, stroke,
+    opacity, box-shadow, transform;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: auto !important;
+
+  color: var(--sd-input-text-default-text-color) !important;
+  background: var(--sd-input-text-default-background-color, white) !important;
+  border: 1px solid var(--sd-input-text-default-border-color) !important;
+
+  border-radius: var(--sd-input-md-border-radius) !important;
+  outline: none;
+
+  // Focus
+  &:focus,
+  &:focus-visible,
+  &:focus-within {
+    outline: none !important;
+    color: var(--sd-input-text-focus-text-color) !important;
+    background: var(--sd-input-text-focus-background-color) !important;
+    box-shadow: var(--sd-input-text-focus-box-shadow) !important;
   }
 
-  .country-selector__list__item {
-    background-color: var(--input-phone-base-list-color) !important;
-    color: var(--input-phone-base-list-text-color) !important;
-    .country-selector__list__item__calling-code {
-      color: var(--input-phone-base-list-text-color) !important;
+  input {
+    border: none !important;
+    border-radius: var(--sd-input-md-border-radius) !important;
+    border: none !important;
+
+    &.input-tel__input {
+      border-top-left-radius: 0 !important;
+      border-bottom-left-radius: 0 !important;
     }
 
-    &.selected {
-      background-color: var(--input-phone-base-selected-color) !important;
-      color: var(--input-phone-base-selected-text-color) !important;
-
-      .country-selector__list__item__calling-code {
-        color: var(--input-phone-base-selected-text-color) !important;
-      }
+    &.country-selector__input {
+      border-top-right-radius: 0 !important;
+      border-bottom-right-radius: 0 !important;
+      border-right: none;
     }
   }
 
-  .input-tel__input {
-    padding-top: 0px !important;
-  }
-  .vue-phone-number-input {
-    border-radius: calc(var(--input-phone-base-border-radius) * 1px);
-    border: 1px solid transparent;
-  }
-  .vue-phone-number-input:focus-within {
-    border-color: var(--input-phone-base-focus-border-color);
-  }
-  .country-selector__input,
-  .input-tel__input {
-    box-shadow: none !important;
-    border-color: var(--input-phone-base-border-color) !important;
-    color: grey;
-    font-size: calc(var(--input-phone-base-font-size) * 1px) !important;
-    font-weight: var(--input-phone-base-font-weight) !important;
-    line-height: 1.5 !important;
-    letter-spacing: var(--input-phone-base-letter-spacing) !important;
-    height: auto !important;
-  }
-
-  .country-selector__input {
-    border-top-left-radius: calc(
-      var(--input-phone-base-border-radius) * 1px
-    ) !important;
-    border-bottom-left-radius: calc(
-      var(--input-phone-base-border-radius) * 1px
-    ) !important;
-  }
-
-  .input-tel__input {
-    border-top-right-radius: calc(
-      var(--input-phone-base-border-radius) * 1px
-    ) !important;
-    border-bottom-right-radius: calc(
-      var(--input-phone-base-border-radius) * 1px
-    ) !important;
-  }
-
+  .country-selector__list,
   .country-selector__list.has-calling-code {
-    border-radius: 0px !important;
-    height: var(--countriesHeight, 300px) !important;
-    min-height: var(--countriesHeight, 300px) !important;
-    max-height: var(--countriesHeight, 300px) !important;
-  }
-  .resize-observer {
-    height: 0px !important;
-  }
-  .input-tel__label {
-    display: none;
-    visibility: hidden;
+    overflow: hidden;
+    color: var(--sd-input-text-default-text-color) !important;
+    background: var(--sd-input-text-default-background-color, white) !important;
+    border: 1px solid var(--sd-input-text-default-border-color) !important;
+    border-radius: var(--sd-input-md-border-radius) !important;
+    outline: none;
+
+    .vue-recycle-scroller__item-wrapper,
+    .vue-recycle-scroller__item-view {
+      border-radius: var(--sd-input-md-border-radius) !important;
+    }
+
+    button {
+      color: var(--sd-input-text-default-text-color) !important;
+      background: var(
+        --sd-input-text-default-background-color,
+        white
+      ) !important;
+    }
   }
 
-  .vue-phone-number-input .input-tel.is-disabled .input-tel__input,
-  .vue-phone-number-input
-    .country-selector.is-disabled
-    .country-selector__input {
-    background: white !important;
+  .country-selector__list__item__flag-container {
+    margin-right: 0 !important;
+  }
+
+  .country-selector__list__item.selected,
+  .country-selector__list__item__calling-code,
+  .vue-recycle-scroller__item-view {
+    color: var(--sd-input-text-default-text-color) !important;
+  }
+
+  .country-selector__list__item__calling-code {
+    width: 41px !important;
   }
 }
 </style>
